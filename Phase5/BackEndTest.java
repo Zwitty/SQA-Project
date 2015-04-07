9bdc4dffa40a7d28d12c996a8de40960f583d4b6
Matthew Clark <matthew@matthewclark.ca>
2015-04-06 21:12:09 -0400

    BackEndTest.java

---
+++ Phase5/BackEndTest.java
@@ -0,0 +1,84 @@
+import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.io.*;
+public class BackEndTest extends TestCase {
+
+    public void testMain() {
+        //fail("Not yet implemented");
+    }
+
+    public void testloadUsers() {
+       // setSeller("James");
+       // assertEquals("James", seller);
+    }
+
+    public void testloadTickets() {
+       // setBuyer("Matt");
+       // asserEquals("Matt", buyer);
+    }
+
+    public void testrunTransactions() {
+        // setPrice(200);
+        // assertEquals(200, price);
+    }
+
+    public void testfindTicket() {
+        // setQuantity(4);
+        // assertEquals(4, quantity);
+    }
+
+    public void testfindTicketPosition() {
+        // setEvent("Nine Inch Nails");
+        // assertEquals("Nine Inch Nails", event);
+    }
+
+    public void testfindUser() {
+        // setSeller("James");
+        // assertEquals("James", getSeller());
+    }
+
+    public void testfindUserPosition() {
+        // setBuyer("Matt");
+        // assertEquals("Matt", getBuyer());
+    }
+
+    public void testbuy() {
+        // setPrice(200);
+        // assertEquals(200, getPrice());
+ }
+
+    public void testsell() {
+        // setQuantity(4);
+        // assertEquals(4, getQuantity());
+    }
+
+    public void testaddCredit() {
+        // setEvent("Nine Inch Nails");
+        // assertEquals("Nine Inch Nails" , getEvent());
+    }
+
+    public void testdeleteUser(){
+
+    }
+
+    public void testcreateUser(){
+
+    }
+
+    public void testrefund(){
+
+    }
+
+    public void writeTo(){
+
+    }
+
+    public void writeUsers(){
+
+    }
+
+    public void writeTickets(){
+
+    }
+}
