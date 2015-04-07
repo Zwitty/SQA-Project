6168d51b62f03882506bffa05b7f1ad39b78d937
Matthew Clark <matthew@matthewclark.ca>
2015-04-06 20:57:38 -0400

    JUnit

---
+++ Phase5/TicketTest.java
@@ -0,0 +1,60 @@
+import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.io.*;
+public class TicketTest extends TestCase {
+
+    public void testTicket() {
+        //fail("Not yet implemented");
+    }
+
+    public void testSetSeller() {
+       // setSeller("James");
+       // assertEquals("James", seller);
+    }
+
+    public void testSetBuyer() {
+       // setBuyer("Matt");
+       // asserEquals("Matt", buyer);
+    }
+
+    public void testSetPrice() {
+        // setPrice(200);
+        // assertEquals(200, price);
+    }
+
+    public void testSetQuantity() {
+        // setQuantity(4);
+        // assertEquals(4, quantity);
+    }
+
+    public void testSetEventName() {
+        // setEvent("Nine Inch Nails");
+        // assertEquals("Nine Inch Nails", event);
+    }
+
+    public void testGetSeller() {
+        // setSeller("James");
+        // assertEquals("James", getSeller());
+    }
+
+    public void testGetBuyer() {
+        // setBuyer("Matt");
+        // assertEquals("Matt", getBuyer());
+    }
+
+    public void testGetPrice() {
+        // setPrice(200);
+        // assertEquals(200, getPrice());
+ }
+
+    public void testGetQuantity() {
+        // setQuantity(4);
+        // assertEquals(4, getQuantity());
+    }
+
+    public void testGetEventName() {
+        // setEvent("Nine Inch Nails");
+        // assertEquals("Nine Inch Nails" , getEvent());
+    }
+}
